7abe9abcfeec0d970ce313a824e1de933303dcde
ps-hwilt <henry.wilt@crisphealth.org>
2023-12-26 14:59:00 -0500

    Reading in a name and printing it back out

--- FirstConsoleApp/src/Program.cs
+++ FirstConsoleApp/src/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine($"Welcome{Environment.NewLine}What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:D} at {currentDate:t}!");
+            Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
