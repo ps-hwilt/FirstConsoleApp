51769b25d0a88c8cc8b055586170c9e814a9efd7
ps-hwilt <henry.wilt@crisphealth.org>
2023-12-26 15:41:18 -0500

    Set up calculator correctly now

--- src/Calculator/Program.cs
+++ src/Calculator/Program.cs
@@ -1,7 +1,11 @@
 ﻿namespace Calculator;
 
-public class Calculator
+public static class Calculator
 {
+    static void Main()
+    {
+        Console.WriteLine("Calc");
+    }
     public static int Add(int x, int y) => x + y;
     public static int Subtract(int x, int y) => x - y;
     public static int Multiply(int x, int y) => x * y;
